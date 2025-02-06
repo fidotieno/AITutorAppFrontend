3b79be880a184d5f247ebc4dbbaa6ea32afbe0df
owalkz <jjaraorie@gmail.com>
2025-02-06 11:40:21 +0300

    Corrected .env references

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -2,25 +2,35 @@
 
 // Register a new user
 const registerUser = async (userData) => {
-  const res = await fetch(process.env.VITE_APP_ENVIRONMENT == 'development' ? "/api/api/auth/register" : `${process.env.VITE_APP_BACKEND_URL}/api/auth/register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
+  const res = await fetch(
+    import.meta.env.VITE_APP_ENVIRONMENT == "development"
+      ? "/api/api/auth/register"
+      : `${import.meta.env.VITE_APP_BACKEND_URL}/api/auth/register`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    }
+  );
   return res.status;
 };
 
 // Login an existing user
 const loginUser = async (userData) => {
-  const res = await fetch(process.env.VITE_APP_ENVIRONMENT == 'development' ? "/api/api/auth/login" : `${process.env.VITE_APP_BACKEND_URL}/api/auth/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
+  const res = await fetch(
+    import.meta.env.VITE_APP_ENVIRONMENT == "development"
+      ? "/api/api/auth/login"
+      : `${import.meta.env.VITE_APP_BACKEND_URL}/api/auth/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    }
+  );
   return res.status;
 };
 
